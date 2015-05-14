ae75381b01b2ab442499a8c968b65a95cac819bb
Tim Watson <tiwatson@gmail.com>
2015-05-14 19:39:42 -0400

    Update where path

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,7 +3,7 @@ class Map < ActiveRecord::Base
 
 
   def d3_tracks(year)
-    DouglasPeucker::LineSimplifier.new(m.map_places.select { |mp| mp.ww_year == year }.map(&:arrival_path).flatten(1).collect { |i| [i[0].to_f, i[1].to_f]}).threshold(0.05).points
+    DouglasPeucker::LineSimplifier.new(self.map_places.select { |mp| mp.ww_year == year }.map(&:arrival_path).flatten(1).collect { |i| [i[0].to_f, i[1].to_f]}).threshold(0.05).points
   end
 
 end
