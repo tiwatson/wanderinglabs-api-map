8569dbbd386a74cd346fa19e3f2cb913e007f322
Tim Watson <tiwatson@gmail.com>
2016-05-12 09:49:50 -0600

    SD

--- lib/import/watsons_wander.rb
+++ lib/import/watsons_wander.rb
@@ -3,14 +3,8 @@ class WatsonsWander
 
 =begin
 
-  MapPlace.create(map_id: 1, title: 'Walmart - Craig', arrived: Date.parse('06-05-2016'), latitude: 40.513195, longitude: -107.569764, price: 0, category: 'PL')
-  load('import/watsons_wander.rb') && Import::WatsonsWander.import('06-05-2016')
-
-  MapPlace.create(map_id: 1, title: 'Walmart - Casper', arrived: Date.parse('07-05-2016'), latitude: 42.818018, longitude: -106.372218, price: 0, category: 'PL')
-  load('import/watsons_wander.rb') && Import::WatsonsWander.import('07-05-2016')
-
-  MapPlace.create(map_id: 1, title: 'Belle Fourche River Campground', arrived: Date.parse('08-05-2016'), latitude: 44.582037, longitude: -104.705549, price: 12, category: 'NP')
-  load('import/watsons_wander.rb') && Import::WatsonsWander.import('08-05-2016')
+  MapPlace.create(map_id: 1, title: 'Spearfish Campground', arrived: Date.parse('11-05-2016'), latitude: 44.479303, longitude: -103.855888, price: 25, category: 'CITY')
+  load('import/watsons_wander.rb') && Import::WatsonsWander.import('11-05-2016')
 
   MapPlace.find_each { |m| m.save }; UpdateInfographic.perform
 
